fb32c91a281faea766c08bd6a40e2f5fced6838b
Shivam Shivam <shivam.sharma1@thoughtworks.com>
2022-03-22 15:34:03 +0530

    [Aditi/Shivam] | Add. Put method to change password.

--- src/main/java/com/booking/users/UserController.java
+++ src/main/java/com/booking/users/UserController.java
@@ -1,7 +1,12 @@
 package com.booking.users;
 
+import com.booking.users.view.models.ChangePasswordRequest;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -12,11 +17,38 @@ import java.util.Map;
 @RestController
 public class UserController {
 
+    private final UserPrincipalService userPrincipalService;
+
+    @Autowired
+    public UserController(UserPrincipalService userPrincipalService) {
+        this.userPrincipalService = userPrincipalService;
+    }
+
     @GetMapping("/login")
     Map<String, Object> login(Principal principal) {
         String username = principal.getName();
         Map<String, Object> userDetails = new HashMap<>();
         userDetails.put("username", username);
         return userDetails;
+    }
+
+    @PutMapping("/changePassword")
+    Map<String, Object> changePassword(Principal principal, @RequestBody ChangePasswordRequest changePasswordRequest) {
+        String username = principal.getName();
+        User user = userPrincipalService.findUserByUsername(username);
+        Map<String, Object> response = new HashMap<>();
+
+        if(!user.getPassword().equals(changePasswordRequest.getOldPassword())) {
+            response.put("status", false);
+            response.put("msg", "Incorrect Old Password");
+            return response;
+        }
+
+        userPrincipalService.updateUserPassword(changePasswordRequest.getNewPassword(), user);
+
+        response.put("status", true);
+        response.put("msg", "Password Updated Successfully");
+
+        return response;
     }
 }
