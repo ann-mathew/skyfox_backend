c752c19626ac4c6e01ee73e5d3742c8c8ac57746
Shivam Shivam <shivam.sharma1@thoughtworks.com>
2022-03-22 15:29:43 +0530

    [Aditi/Shivam] | Add. Repository method to update the password.

--- src/main/java/com/booking/users/UserRepository.java
+++ src/main/java/com/booking/users/UserRepository.java
@@ -1,6 +1,7 @@
 package com.booking.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    User save(User user);
 }
