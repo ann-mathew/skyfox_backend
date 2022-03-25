9bb55cc865189400eb7e0d2eba318f082e1ddd27
Shivam Shivam <shivam.sharma1@thoughtworks.com>
2022-03-26 03:34:01 +0530

    [Aditi/Shivam] | Add. data seeder for admins in pasword_history

--- src/main/java/com/booking/DataSeeder.java
+++ src/main/java/com/booking/DataSeeder.java
@@ -1,23 +1,33 @@
 package com.booking;
 
+import com.booking.passwordHistory.repository.PasswordHistory;
+import com.booking.passwordHistory.repository.PasswordHistoryRepository;
 import com.booking.users.User;
 import com.booking.users.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Configuration
 public class DataSeeder {
 
     @Bean
-    CommandLineRunner initDatabase(UserRepository repository) {
+    CommandLineRunner initDatabase(UserRepository repository, PasswordHistoryRepository passwordHistoryRepository) {
         return args -> {
             if (repository.findByUsername("seed-user-1").isEmpty()) {
                 repository.save(new User("seed-user-1", "foobar", "name","abcd@gmail.com", "2454634623", "12","admin"));
             }
             if (repository.findByUsername("seed-user-2").isEmpty()) {
                 repository.save(new User("seed-user-2", "foobar",  "name","abcd@gmail.com", "2454634623", "12","admin"));
+            }
+            if (passwordHistoryRepository.findByUsername("seed-user-1").isEmpty()) {
+                passwordHistoryRepository.save(new PasswordHistory("seed-user-1", "foobar", Timestamp.from(Instant.now())));
+            }
+            if (passwordHistoryRepository.findByUsername("seed-user-2").isEmpty()) {
+                passwordHistoryRepository.save(new PasswordHistory("seed-user-2", "foobar", Timestamp.from(Instant.now())));
             }
         };
     }
